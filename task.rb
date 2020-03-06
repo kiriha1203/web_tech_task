561da7a592c7f92ba84bf3225b941ffc7ad36880
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-03-06 21:41:57 +0900

    fix

--- task.rb
+++ task.rb
@@ -7,10 +7,10 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/task', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 # この一行を追記
 server.mount('/goya1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya1.rb')
 server.mount('/goya2.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya2.rb')
 server.mount('/goya3.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya3.rb')
-server.start
+serverç
